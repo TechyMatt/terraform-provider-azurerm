dba9f3221d323ddb08577f31c4ddbc5215705041
Matt Betts <20343261+mb290@users.noreply.github.com>
2022-05-04 17:26:31 -0400

    Enable allow_branch_to_branch_traffic on azurerm_virtual_hub
    This adds support for allow_branch_to_branch_traffic on azurerm_virtual_hub resource. This is an optional setting when deploying an Azure Route Server, however shouldn't be set if using a virtual hub with Virtual Wan.

--- internal/services/network/virtual_wan_resource.go
+++ internal/services/network/virtual_wan_resource.go
@@ -1,6 +1,7 @@
 package network
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"time"
@@ -9,9 +10,11 @@ import (
 	"github.com/hashicorp/go-azure-helpers/lang/response"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/azure"
 	"github.com/hashicorp/terraform-provider-azurerm/helpers/tf"
+	"github.com/hashicorp/terraform-provider-azurerm/helpers/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/clients"
-	"github.com/hashicorp/terraform-provider-azurerm/internal/features"
+	"github.com/hashicorp/terraform-provider-azurerm/internal/locks"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/services/network/parse"
+	networkValidate "github.com/hashicorp/terraform-provider-azurerm/internal/services/network/validate"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tags"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/pluginsdk"
 	"github.com/hashicorp/terraform-provider-azurerm/internal/tf/validation"
@@ -19,147 +22,212 @@ import (
 	"github.com/hashicorp/terraform-provider-azurerm/utils"
 )
 
-func resourceVirtualWan() *pluginsdk.Resource {
+const virtualHubResourceName = "azurerm_virtual_hub"
+
+func resourceVirtualHub() *pluginsdk.Resource {
 	return &pluginsdk.Resource{
-		Create: resourceVirtualWanCreateUpdate,
-		Read:   resourceVirtualWanRead,
-		Update: resourceVirtualWanCreateUpdate,
-		Delete: resourceVirtualWanDelete,
+		Create: resourceVirtualHubCreateUpdate,
+		Read:   resourceVirtualHubRead,
+		Update: resourceVirtualHubCreateUpdate,
+		Delete: resourceVirtualHubDelete,
 		Importer: pluginsdk.ImporterValidatingResourceId(func(id string) error {
-			_, err := parse.VirtualWanID(id)
+			_, err := parse.VirtualHubID(id)
 			return err
 		}),
 
 		Timeouts: &pluginsdk.ResourceTimeout{
-			Create: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Create: pluginsdk.DefaultTimeout(60 * time.Minute),
 			Read:   pluginsdk.DefaultTimeout(5 * time.Minute),
-			Update: pluginsdk.DefaultTimeout(30 * time.Minute),
-			Delete: pluginsdk.DefaultTimeout(30 * time.Minute),
+			Update: pluginsdk.DefaultTimeout(60 * time.Minute),
+			Delete: pluginsdk.DefaultTimeout(60 * time.Minute),
 		},
 
-		Schema: resourceVirtualWanSchema(),
+		Schema: map[string]*pluginsdk.Schema{
+			"name": {
+				Type:         pluginsdk.TypeString,
+				Required:     true,
+				ForceNew:     true,
+				ValidateFunc: networkValidate.VirtualHubName,
+			},
+
+			"resource_group_name": azure.SchemaResourceGroupName(),
+
+			"location": azure.SchemaLocation(),
+
+			"address_prefix": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: validate.CIDR,
+			},
+
+			"sku": {
+				Type:     pluginsdk.TypeString,
+				Optional: true,
+				ForceNew: true,
+				ValidateFunc: validation.StringInSlice([]string{
+					"Basic",
+					"Standard",
+				}, false),
+			},
+
+			"virtual_wan_id": {
+				Type:         pluginsdk.TypeString,
+				Optional:     true,
+				ForceNew:     true,
+				ValidateFunc: azure.ValidateResourceID,
+			},
+
+			"virtual_router_asn": {
+				Type:     pluginsdk.TypeInt,
+				Computed: true,
+			},
+
+			"virtual_router_ips": {
+				Type:     pluginsdk.TypeList,
+				Computed: true,
+				Elem: &pluginsdk.Schema{
+					Type: pluginsdk.TypeString,
+				},
+			},
+
+			"route": {
+				Type:     pluginsdk.TypeSet,
+				Optional: true,
+				Elem: &pluginsdk.Resource{
+					Schema: map[string]*pluginsdk.Schema{
+						"address_prefixes": {
+							Type:     pluginsdk.TypeList,
+							Required: true,
+							Elem: &pluginsdk.Schema{
+								Type:         pluginsdk.TypeString,
+								ValidateFunc: validate.CIDR,
+							},
+						},
+						"next_hop_ip_address": {
+							Type:         pluginsdk.TypeString,
+							Required:     true,
+							ValidateFunc: validate.IPv4Address,
+						},
+					},
+				},
+			},
+
+			"allow_branch_to_branch_traffic": {
+				Type:     pluginsdk.TypeBool,
+				Optional: true,
+				Default:  false,
+				ConflictsWith: []string{
+					"virtual_wan_id",
+				},
+			},
+
+			"tags": tags.Schema(),
+
+			"default_route_table_id": {
+				Type:     pluginsdk.TypeString,
+				Computed: true,
+			},
+		},
 	}
 }
 
-func resourceVirtualWanSchema() map[string]*pluginsdk.Schema {
-	out := map[string]*pluginsdk.Schema{
-		"name": {
-			Type:         pluginsdk.TypeString,
-			Required:     true,
-			ForceNew:     true,
-			ValidateFunc: validation.StringIsNotEmpty,
-		},
-
-		"resource_group_name": azure.SchemaResourceGroupName(),
-
-		"location": azure.SchemaLocation(),
-
-		"disable_vpn_encryption": {
-			Type:     pluginsdk.TypeBool,
-			Optional: true,
-			Default:  false,
-		},
-
-		"allow_branch_to_branch_traffic": {
-			Type:     pluginsdk.TypeBool,
-			Optional: true,
-			Default:  true,
-		},
-
-		"office365_local_breakout_category": {
-			Type:     pluginsdk.TypeString,
-			Optional: true,
-			ValidateFunc: validation.StringInSlice([]string{
-				string(network.OfficeTrafficCategoryAll),
-				string(network.OfficeTrafficCategoryNone),
-				string(network.OfficeTrafficCategoryOptimize),
-				string(network.OfficeTrafficCategoryOptimizeAndAllow),
-			}, false),
-			Default: string(network.OfficeTrafficCategoryNone),
-		},
-
-		"type": {
-			Type:     pluginsdk.TypeString,
-			Optional: true,
-			Default:  "Standard",
-		},
-
-		"tags": tags.Schema(),
-	}
-
-	if !features.ThreePointOhBeta() {
-		out["allow_vnet_to_vnet_traffic"] = &pluginsdk.Schema{
-			Type:       pluginsdk.TypeBool,
-			Optional:   true,
-			Default:    false,
-			Deprecated: "this property has been removed from the API and will be removed in version 3.0 of the provider",
-		}
-	}
-
-	return out
-}
-
-func resourceVirtualWanCreateUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
-	client := meta.(*clients.Client).Network.VirtualWanClient
+func resourceVirtualHubCreateUpdate(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Network.VirtualHubClient
 	subscriptionId := meta.(*clients.Client).Account.SubscriptionId
 	ctx, cancel := timeouts.ForCreateUpdate(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	log.Printf("[INFO] preparing arguments for Virtual WAN creation.")
+	if _, ok := ctx.Deadline(); !ok {
+		return fmt.Errorf("deadline is not properly set for Virtual Hub")
+	}
 
-	id := parse.NewVirtualWanID(subscriptionId, d.Get("resource_group_name").(string), d.Get("name").(string))
+	id := parse.NewVirtualHubID(subscriptionId, d.Get("resource_group_name").(string), d.Get("name").(string))
 
-	location := azure.NormalizeLocation(d.Get("location").(string))
-	disableVpnEncryption := d.Get("disable_vpn_encryption").(bool)
-	allowBranchToBranchTraffic := d.Get("allow_branch_to_branch_traffic").(bool)
-	office365LocalBreakoutCategory := d.Get("office365_local_breakout_category").(string)
-	virtualWanType := d.Get("type").(string)
-	t := d.Get("tags").(map[string]interface{})
+	locks.ByName(id.Name, virtualHubResourceName)
+	defer locks.UnlockByName(id.Name, virtualHubResourceName)
 
 	if d.IsNewResource() {
 		existing, err := client.Get(ctx, id.ResourceGroup, id.Name)
 		if err != nil {
 			if !utils.ResponseWasNotFound(existing.Response) {
-				return fmt.Errorf("checking for presence of existing %s: %+v", id, err)
+				return fmt.Errorf("checking for present of existing %s: %+v", id, err)
 			}
 		}
-
 		if !utils.ResponseWasNotFound(existing.Response) {
-			return tf.ImportAsExistsError("azurerm_virtual_wan", id.ID())
+			return tf.ImportAsExistsError("azurerm_virtual_hub", id.ID())
 		}
 	}
 
-	wan := network.VirtualWAN{
+	location := azure.NormalizeLocation(d.Get("location").(string))
+	route := d.Get("route").(*pluginsdk.Set).List()
+	allowBranchToBranchTraffic := d.Get("allow_branch_to_branch_traffic").(bool)
+	t := d.Get("tags").(map[string]interface{})
+
+	parameters := network.VirtualHub{
 		Location: utils.String(location),
-		Tags:     tags.Expand(t),
-		VirtualWanProperties: &network.VirtualWanProperties{
-			DisableVpnEncryption:           utils.Bool(disableVpnEncryption),
+		VirtualHubProperties: &network.VirtualHubProperties{
 			AllowBranchToBranchTraffic:     utils.Bool(allowBranchToBranchTraffic),
-			Office365LocalBreakoutCategory: network.OfficeTrafficCategory(office365LocalBreakoutCategory),
-			Type:                           utils.String(virtualWanType),
+			RouteTable: expandVirtualHubRoute(route),
 		},
+		Tags: tags.Expand(t),
 	}
 
-	future, err := client.CreateOrUpdate(ctx, id.ResourceGroup, id.Name, wan)
+	if v, ok := d.GetOk("address_prefix"); ok {
+		parameters.VirtualHubProperties.AddressPrefix = utils.String(v.(string))
+	}
+
+	
+
+	if v, ok := d.GetOk("sku"); ok {
+		parameters.VirtualHubProperties.Sku = utils.String(v.(string))
+	}
+
+	if v, ok := d.GetOk("virtual_wan_id"); ok {
+		parameters.VirtualHubProperties.VirtualWan = &network.SubResource{
+			ID: utils.String(v.(string)),
+		}
+	}
+
+	future, err := client.CreateOrUpdate(ctx, id.ResourceGroup, id.Name, parameters)
 	if err != nil {
-		return fmt.Errorf("creating/updating %s: %+v", id, err)
+		return fmt.Errorf("creating %s: %+v", id, err)
 	}
 
-	if err = future.WaitForCompletionRef(ctx, client.Client); err != nil {
-		return fmt.Errorf("waiting for creation/update of %s: %+v", id, err)
+	if err := future.WaitForCompletionRef(ctx, client.Client); err != nil {
+		return fmt.Errorf("waiting for creation of %s: %+v", id, err)
+	}
+
+	// Hub returns provisioned while the routing state is still "provisining". This might cause issues with following hubvnet connection operations.
+	// https://github.com/Azure/azure-rest-api-specs/issues/10391
+	// As a workaround, we will poll the routing state and ensure it is "Provisioned".
+
+	// deadline is checked at the entry point of this function
+	timeout, _ := ctx.Deadline()
+	stateConf := &pluginsdk.StateChangeConf{
+		Pending:                   []string{"Provisioning"},
+		Target:                    []string{"Provisioned", "Failed", "None"},
+		Refresh:                   virtualHubCreateRefreshFunc(ctx, client, id.ResourceGroup, id.Name),
+		PollInterval:              15 * time.Second,
+		ContinuousTargetOccurence: 3,
+		Timeout:                   time.Until(timeout),
+	}
+	_, err = stateConf.WaitForStateContext(ctx)
+	if err != nil {
+		return fmt.Errorf("waiting for %s provisioning route: %+v", id, err)
 	}
 
 	d.SetId(id.ID())
 
-	return resourceVirtualWanRead(d, meta)
+	return resourceVirtualHubRead(d, meta)
 }
 
-func resourceVirtualWanRead(d *pluginsdk.ResourceData, meta interface{}) error {
-	client := meta.(*clients.Client).Network.VirtualWanClient
+func resourceVirtualHubRead(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Network.VirtualHubClient
 	ctx, cancel := timeouts.ForRead(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := parse.VirtualWanID(d.Id())
+	id, err := parse.VirtualHubID(d.Id())
 	if err != nil {
 		return err
 	}
@@ -167,12 +235,11 @@ func resourceVirtualWanRead(d *pluginsdk.ResourceData, meta interface{}) error {
 	resp, err := client.Get(ctx, id.ResourceGroup, id.Name)
 	if err != nil {
 		if utils.ResponseWasNotFound(resp.Response) {
-			log.Printf("[DEBUG] %s was not found - removing from state", *id)
+			log.Printf("[INFO] Virtual Hub %q does not exist - removing from state", d.Id())
 			d.SetId("")
 			return nil
 		}
-
-		return fmt.Errorf("making Read request on %s: %+v", *id, err)
+		return fmt.Errorf("reading %s: %+v", *id, err)
 	}
 
 	d.Set("name", id.Name)
@@ -180,42 +247,142 @@ func resourceVirtualWanRead(d *pluginsdk.ResourceData, meta interface{}) error {
 	if location := resp.Location; location != nil {
 		d.Set("location", azure.NormalizeLocation(*location))
 	}
-
-	if props := resp.VirtualWanProperties; props != nil {
-		d.Set("disable_vpn_encryption", props.DisableVpnEncryption)
-		d.Set("allow_branch_to_branch_traffic", props.AllowBranchToBranchTraffic)
-		d.Set("office365_local_breakout_category", props.Office365LocalBreakoutCategory)
-		if !features.ThreePointOhBeta() {
-			d.Set("allow_vnet_to_vnet_traffic", false)
+	if props := resp.VirtualHubProperties; props != nil {
+		d.Set("address_prefix", props.AddressPrefix)
+		d.Set("sku", props.Sku)
+		
+		if err := d.Set("route", flattenVirtualHubRoute(props.RouteTable)); err != nil {
+			return fmt.Errorf("setting `route`: %+v", err)
 		}
-		d.Set("type", props.Type)
+
+		var allowBranchToBranchTraffic *bool
+		if props.AllowBranchToBranchTraffic != nil{
+			allowBranchToBranchTraffic = props.AllowBranchToBranchTraffic
+		}
+		d.Set("allow_branch_to_branch_traffic", allowBranchToBranchTraffic)
+
+
+		var virtualWanId *string
+		if props.VirtualWan != nil {
+			virtualWanId = props.VirtualWan.ID
+		}
+		d.Set("virtual_wan_id", virtualWanId)
+
+		var virtualRouterAsn *int64
+		if props.VirtualRouterAsn != nil {
+			virtualRouterAsn = props.VirtualRouterAsn
+		}
+		d.Set("virtual_router_asn", virtualRouterAsn)
+
+		var virtualRouterIps *[]string
+		if props.VirtualRouterIps != nil {
+			virtualRouterIps = props.VirtualRouterIps
+		}
+		d.Set("virtual_router_ips", virtualRouterIps)
 	}
+
+	defaultRouteTable := parse.NewHubRouteTableID(id.SubscriptionId, id.ResourceGroup, id.Name, "defaultRouteTable")
+	d.Set("default_route_table_id", defaultRouteTable.ID())
 
 	return tags.FlattenAndSet(d, resp.Tags)
 }
 
-func resourceVirtualWanDelete(d *pluginsdk.ResourceData, meta interface{}) error {
-	client := meta.(*clients.Client).Network.VirtualWanClient
+func resourceVirtualHubDelete(d *pluginsdk.ResourceData, meta interface{}) error {
+	client := meta.(*clients.Client).Network.VirtualHubClient
 	ctx, cancel := timeouts.ForDelete(meta.(*clients.Client).StopContext, d)
 	defer cancel()
 
-	id, err := parse.VirtualWanID(d.Id())
+	id, err := parse.VirtualHubID(d.Id())
 	if err != nil {
 		return err
 	}
 
+	locks.ByName(id.Name, virtualHubResourceName)
+	defer locks.UnlockByName(id.Name, virtualHubResourceName)
+
 	future, err := client.Delete(ctx, id.ResourceGroup, id.Name)
 	if err != nil {
-		// deleted outside of Terraform
-
 		return fmt.Errorf("deleting %s: %+v", *id, err)
 	}
 
 	if err = future.WaitForCompletionRef(ctx, client.Client); err != nil {
 		if !response.WasNotFound(future.Response()) {
-			return fmt.Errorf("waiting for the deletion of %s: %+v", *id, err)
+			return fmt.Errorf("waiting for deleting %s: %+v", *id, err)
 		}
 	}
 
 	return nil
+}
+
+func expandVirtualHubRoute(input []interface{}) *network.VirtualHubRouteTable {
+	if len(input) == 0 {
+		return nil
+	}
+
+	results := make([]network.VirtualHubRoute, 0)
+	for _, item := range input {
+		if item == nil {
+			continue
+		}
+
+		v := item.(map[string]interface{})
+		addressPrefixes := v["address_prefixes"].([]interface{})
+		nextHopIpAddress := v["next_hop_ip_address"].(string)
+
+		results = append(results, network.VirtualHubRoute{
+			AddressPrefixes:  utils.ExpandStringSlice(addressPrefixes),
+			NextHopIPAddress: utils.String(nextHopIpAddress),
+		})
+	}
+
+	result := network.VirtualHubRouteTable{
+		Routes: &results,
+	}
+
+	return &result
+}
+
+func flattenVirtualHubRoute(input *network.VirtualHubRouteTable) []interface{} {
+	results := make([]interface{}, 0)
+	if input == nil || input.Routes == nil {
+		return results
+	}
+
+	for _, item := range *input.Routes {
+		addressPrefixes := utils.FlattenStringSlice(item.AddressPrefixes)
+		nextHopIpAddress := ""
+
+		if item.NextHopIPAddress != nil {
+			nextHopIpAddress = *item.NextHopIPAddress
+		}
+
+		results = append(results, map[string]interface{}{
+			"address_prefixes":    addressPrefixes,
+			"next_hop_ip_address": nextHopIpAddress,
+		})
+	}
+
+	return results
+}
+
+func virtualHubCreateRefreshFunc(ctx context.Context, client *network.VirtualHubsClient, resourceGroup, name string) pluginsdk.StateRefreshFunc {
+	return func() (interface{}, string, error) {
+		res, err := client.Get(ctx, resourceGroup, name)
+		if err != nil {
+			if utils.ResponseWasNotFound(res.Response) {
+				return nil, "", fmt.Errorf("Virtual Hub %q (Resource Group %q) doesn't exist", resourceGroup, name)
+			}
+
+			return nil, "", fmt.Errorf("retrieving Virtual Hub %q (Resource Group %q): %+v", resourceGroup, name, err)
+		}
+		if res.VirtualHubProperties == nil {
+			return nil, "", fmt.Errorf("unexpected nil properties of Virtual Hub %q (Resource Group %q)", resourceGroup, name)
+		}
+
+		state := res.VirtualHubProperties.RoutingState
+		if state == "Failed" {
+			return nil, "", fmt.Errorf("failed to provision routing on Virtual Hub %q (Resource Group %q)", resourceGroup, name)
+		}
+		return res, string(res.VirtualHubProperties.RoutingState), nil
+	}
 }
